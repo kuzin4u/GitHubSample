c442ac45b7452feab958913136a5f36f22c50096
Сергей Кузин <sergeykuzin@MacBook-Pro-Sergej.local>
2017-12-08 23:45:41 +0000

    Initial Commit - GitHub Project
    GitHub Project Created !

---
+++ GitHubProject/GitHubProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitHubProject
+//
+//  Created by Сергей Кузин on 08.12.2017.
+//  Copyright © 2017 Сергей Кузин. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
